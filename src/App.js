2bc626caffab81ba6cd0d3406f0fc7bef1b4c970
Aaron Everette <aeverette0120@gmail.com>
2022-09-20 21:52:10 -0400

    created a test to pull up user name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] =useState('')
+
+  useEffect (() => {
+    fetch('https://api.github.com/users/Aeverette0120')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubName(data.name)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{ gitHubName }</h2>
     </div>
   );
 }
